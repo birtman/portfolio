52fcf6491a6e3276a06c7553e2e95ef277747ddc
Birte Spreuer <mail@birtespreuer.com>
2020-10-04 18:42:45 +0200

    trying to make it work with Redux5

--- src/components/ProjectDetail.js
+++ src/components/ProjectDetail.js
@@ -1,14 +1,22 @@
 import React from "react";
 import Nav from "./Nav";
 import Footer from "./Footer";
+import { connect } from "react-redux";
+import { Link, Redirect } from "react-router-dom";
 
-const ProjectDetails = (props) => {
+export function ProjectDetails(props) {
   return (
     <div>
       <h1 className="">{props.project.title}</h1>
       <Footer />
     </div>
   );
+}
+
+const mapStateToProps = (state) => {
+  return {
+    project: state.selected.project,
+  };
 };
 
-export default ProjectDetails;
+export default connect(mapStateToProps)(ProjectDetails);
