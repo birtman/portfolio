7f3658a633ea2372cefbf6f831607f530fb7acba
Birte Spreuer <mail@birtespreuer.com>
2020-10-04 18:38:50 +0200

    trying to make it work with Redux3

--- src/components/ProjectDetail.js
+++ src/components/ProjectDetail.js
@@ -2,9 +2,10 @@ import React from "react";
 import Nav from "./Nav";
 import Footer from "./Footer";
 
-const ProjectDetails = () => {
+const ProjectDetails = (props) => {
   return (
     <div>
+      <h1 className="">{props.project.title}</h1>
       <Footer />
     </div>
   );
